fad828293fd6efaccef288744a5d1d2aea1c85e5
Hemel17 <redewanmahmud@gmail.com>
2024-04-16 00:09:03 +0600

    design added

--- scripts/app.js
+++ scripts/app.js
@@ -51,16 +51,64 @@ const allSeats = [
   "J4",
 ];
 
+const bookedSeats = [];
 const seatsContainer = document.getElementById("seats");
+const nextBtn = document.getElementById("next");
+
+
+
+const hello = (seat) => {
+  bookedSeats.push(seat);
+  if(bookedSeats.length === 4){
+    nextBtn.removeAttribute('disabled')
+  } else {
+    nextBtn.setAttribute('disabled', 'disabled')
+  }
+  console.log(bookedSeats);
+};
 
 const seatsHtml = allSeats
   .map((seat) => {
-    return `<div class="p-4 font-medium text-lg bg-gray-200 flex justify-center items-center rounded-xl">
+    const bgClass = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"].includes(
+      seat
+    )
+      ? ""
+      : "bg-gray-200";
+    const hoverClasses = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+    ].includes(seat)
+      ? ""
+      : "hover:cursor-pointer hover:bg-green-200";
+    const clickEvent = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+    ].includes(seat)
+      ? ""
+      : `onclick="hello('${seat}')"`;
+
+    return `<div class="p-4 font-medium text-lg ${bgClass} flex justify-center items-center rounded-xl ${hoverClasses}" id="${seat}" ${clickEvent}>
     <p>${seat}</p>
     </div>`;
   })
   .join("");
 
-  seatsContainer.innerHTML = seatsHtml;
+seatsContainer.innerHTML = seatsHtml;
 
 console.log("app is running");
